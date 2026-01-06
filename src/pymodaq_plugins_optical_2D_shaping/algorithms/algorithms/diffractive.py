02f438634c630ae094d1a5c74b7e82f243cfc394
Sébastien Weber <seba.weber@gmail.com>
2026-01-06 21:06:05 +0100

    other algorithms

--- src/pymodaq_plugins_optical_2D_shaping/algorithms/algorithms/diffractive.py
+++ src/pymodaq_plugins_optical_2D_shaping/algorithms/algorithms/diffractive.py
@@ -5,6 +5,7 @@ Created the 20/07/2023
 @author: Sebastien Weber
 """
 from abc import ABC, abstractmethod
+from functools import cache
 from pathlib import Path
 from typing import Union, Tuple, List, Callable, TYPE_CHECKING
 
@@ -27,16 +28,60 @@ logger = set_logger(get_module_name(__file__))
 plugin_config = PluginConfig()
 
 
+def approx_inverse_f(y: np.ndarray):
+    return (2 * y + 3 / 10 * y ** 3 + 321 / 2800 * y ** 5 + 3197 / 56000 * y ** 7
+    +445617 / 13798400 * y ** 9
+    +1766784699 / 89689600000 * y ** 11 + 317184685563 / 25113088000000 * y ** 13
+    +14328608561991 / 1707689984000000 * y ** 15
+    +6670995251837391 / 1165411287040000000 * y ** 17
+    +910588298588385889 / 228420612259840000000 * y ** 19
+    +1889106915501879285127263 / 669318078043783168000000000 * y ** 21
+    +122684251268939994619239 / 60571771768668160000000000 * y ** 23
+    +86578199631805319180104483967 / 58899990867852918784000000000000 * y ** 25
+    +36790913563978761395277930686421 /
+    34161994703354692894720000000000000 * y ** 27
+    +295479400033606079171291233070109663 /
+    371437974410411888261201920000000000000 * y ** 29
+    +5429197579977012936689051219262425180781 /
+    9174517967937173640051687424000000000000000 * y ** 31
+    +111912180845235829957717919886518013347855667 /
+    252655431286439247725093999083520000000000000000 * y ** 33
+    +21623582556767547163123245489615552651038372109 /
+    64865414807824606864932296091238400000000000000000 * y ** 35
+    +1686950689722579328034933293949678511289600201851 /
+    6691379632807169971329857912569856000000000000000000 * y ** 37
+    +362964877894310955248925785551248746981943835416147 /
+    1895485357802467420969439750506151936000000000000000000 * y ** 39
+    +241119375769652087142546687133690376324455849576103305306113 /
+    1651328495419246089263940926464174667092459520000000000000000000 * y ** 41
+    +62733744440681157939079200023293588467527441653821853933262223 /
+    561451688442543670349739914997819386811436236800000000000000000000 * y ** 43
+    +90541073261492600342265407374539407149059862146954660576097727343 /
+    1055529174271982100257511040195900447205500125184000000000000000000000
+    *y ** 45
+    +126749063504538759034999649107808731423757688715140874157349767781 /
+    1919143953221785636831838254901637176737272954880000000000000000000000
+    *y ** 47
+    +249693053510060031661928074632553530869877798468407353253082594529911 /
+    4897091793534510236175803834629896837864823539630080000000000000000000000
+    *y ** 49)
+
+
+def approx_inverse_sinc(x: np.ndarray) -> np.ndarray:
+    return np.sqrt(3/2) * approx_inverse_f(np.sqrt(1 - x))
+
 
 class PhasePattern(ABC):
     
     NAME: str = None  # to be reimplemented
     METHOD: str = None  # letter from the method description in the DOI:10.1364/OE.24.006249 | OPTICS EXPRESS 6253 paper
 
-    def __init__(self, input_field: Field, target_field: Field, gamma: int):
+    def __init__(self, input_field: Field, target_field: Field, gamma: int, **kwargs):
         self.input_field = input_field
         self.target_field = target_field
         self.gamma = gamma
+
+        self.other_kwargs = kwargs
 
     @abstractmethod
     def generate_phase(self) -> np.ndarray:
@@ -46,6 +91,14 @@ class PhasePattern(ABC):
     def generate_grating_phase(self) -> np.ndarray:
         x_array = np.linspace(0, self.input_field.shape[1], self.input_field.shape[1])
         return (2 * np.pi * x_array / self.gamma) % 2*np.pi
+
+    def relative_amplitude(self) -> np.ndarray:
+        relative_amplitude = ((self.target_field.amplitude / self.target_field.amplitude.max()) /
+                              (self.input_field.amplitude / self.input_field.amplitude.max()))
+        return relative_amplitude / relative_amplitude.max()
+
+    def relative_phase(self) -> np.ndarray:
+        return self.target_field.phase - self.input_field.phase + self.generate_grating_phase()
 
 
 class PatternFactory:
@@ -88,18 +141,13 @@ class PatternFactory:
         return list(self._registry.keys())
 
 
-
-
 @PatternFactory.register()
 class RelativeAmplitude(PhasePattern):
     NAME = 'Relative Amplitude'
     METHOD = 'A'
 
     def generate_phase(self) -> np.ndarray:
-        relative_amplitude = ((self.target_field.amplitude / self.target_field.amplitude.max()) /
-                              (self.input_field.amplitude / self.input_field.amplitude.max()))
-        relative_amplitude = relative_amplitude / relative_amplitude.max()
-        return relative_amplitude * self.generate_grating_phase()
+        return self.relative_amplitude() * self.relative_phase()
 
 
 @PatternFactory.register()
@@ -115,6 +163,37 @@ class Scattering(PhasePattern):
                         np.exp(1j * (self.target_field.phase +  self.generate_grating_phase())))
         phase = interference.phase
         return phase
+
+
+@PatternFactory.register()
+class Davis(PhasePattern):
+    NAME = 'Davis'
+    METHOD = 'C'
+
+    def generate_phase(self) -> np.ndarray:
+        return (1 - 1 / np.pi * approx_inverse_sinc(self.relative_amplitude())) * self.relative_phase()
+
+
+@PatternFactory.register()
+class Bolduc(PhasePattern):
+    NAME = 'Bolduc'
+    METHOD = 'D'
+
+    def generate_phase(self) -> np.ndarray:
+        Mfactor = (1 + 1 / np.pi * approx_inverse_sinc(self.relative_amplitude()))
+        return Mfactor * (self.relative_phase() - np.pi * Mfactor)
+
+
+@PatternFactory.register()
+class Arrizon(PhasePattern):
+    NAME = 'Arrizon'
+    METHOD = 'E'
+
+    def generate_phase(self) -> np.ndarray:
+        Mfactor = (1 - 1 / np.pi * approx_inverse_sinc(self.relative_amplitude()))
+        return Mfactor * (self.relative_phase() - np.pi * Mfactor)
+
+
 
 
 
@@ -140,6 +219,7 @@ class OE2016(AlgoBase):
          'tip': 'Grating period of the modulation in pixels'},
         {'title': 'Diffractive Models', 'name': 'diff_model', 'type': 'list', 'value': diff_factory.keys[0],
          'limits': diff_factory.keys},
+        {'title': 'Order N', 'name': 'order_n', 'type': 'int', 'value': 10,}
     ]
 
     def __init__(self, parent: 'AlgoApp' = None):
@@ -150,7 +230,8 @@ class OE2016(AlgoBase):
         diff_phase = diff_factory.get_phase(self.settings['diff_model'],
                                             input_field=self._input_field,
                                             target_field=self._target_field,
-                                            gamma=self.settings['gamma'])
+                                            gamma=self.settings['gamma'],
+                                            order_n = self.settings['order_n'])
 
         self.set_phase_in_object_plane(diff_phase)
 
