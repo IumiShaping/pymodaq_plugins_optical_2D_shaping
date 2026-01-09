162ecb4f58d1af99c5a86e7e44bcdf5ef4122d76
Sébastien Weber <seba.weber@gmail.com>
2026-01-09 20:28:15 +0100

    tests

--- src/pymodaq_plugins_optical_2D_shaping/algorithms/algorithms/conjugate_gradient.py
+++ src/pymodaq_plugins_optical_2D_shaping/algorithms/algorithms/conjugate_gradient.py
@@ -2,6 +2,8 @@
 """
 Created the 20/07/2023
 
+see https://gregorygundersen.com/blog/2022/03/20/conjugate-gradient-descent/
+
 @author: Sebastien Weber
 """
 from pathlib import Path
@@ -10,6 +12,8 @@ from qtpy import QtWidgets, QtCore
 
 import numpy as np
 
+from pymodaq_gui.utils import DockArea
+from pymodaq_gui.utils.utils import mkQApp
 from pymodaq_utils.logger import set_logger, get_module_name
 from pymodaq_gui.parameter import Parameter
 
@@ -17,12 +21,12 @@ from pymodaq_plugins_optical_2D_shaping.algorithms.factory import AlgorithmFacto
 from pymodaq_plugins_optical_2D_shaping.algorithms.utils import AlgoBase, Field, LensSetup, ApplyMaskTo
 from pymodaq_plugins_optical_2D_shaping.utils import Config as PluginConfig
 
-import torch
-import torch.optim as optim
-import ncg_optimizer as optim_ncg
-import torch.nn as nn
-import torch.fft
-import torch.nn.functional as F
+from pytensor import tensor as tensor
+from pytensor.tensor import fft
+import pytensor as pt
+from pytensor.compile.io import In
+
+from scipy.fft import fft2
 
 if TYPE_CHECKING:
     from pymodaq_plugins_optical_2D_shaping.algorithms.algorithm_app import AlgoApp
@@ -31,108 +35,179 @@ logger = set_logger(get_module_name(__file__))
 plugin_config = PluginConfig()
 
 
-@AlgorithmFactory.register_algorithm()
-class ConjugateGradient(AlgoBase):
-    """ Implementation of the ConjugateGradient iterative algorithm to create amplitude and phase modulated
-    image with phase only spatial light modulators in the Fourier plane of a converging lens
+class Model:
+    """ Creates the model Parameter: the phase distribution from an initial phase
 
-    Based on Vol. 25, No. 10 | 15 May 2017 | OPTICS EXPRESS 11695 and implemented here with pytorch
+    Attributes:
+    -----------
+    phase_distribution:
+        The tensor parameter to evolve
+    phase_array: np.ndarray
+        ReadOnly
 
-    The corresponding experimental setup should define a working light wavelength and a focal length
-    of the used lens
+    The Tensor and the array share the same memory
     """
 
-    ALGO_NAME = 'ConjugateGradient'
-    SETUP_TYPE = LensSetup.TwoF
-    ITERATIVE = True
+    def __init__(self, object_field: Field, target_field: Field):
+        super().__init__()
+        self.target_field = target_field
 
-    params = []
+        # object_field = object_field.pad(
+        #     ((int(target_field.shape[0] / 2), int(target_field.shape[0] / 2)),
+        #      (int(target_field.shape[1] / 2), int(target_field.shape[1] / 2)))
+        # )
+        self._object_amplitude_array = object_field.amplitude
+        self._phase_distribution = pt.shared(object_field.phase, 'phase')
 
-    def __init__(self, parent: 'AlgoApp' = None):
-        super().__init__(parent)
-        self._slices: tuple[slice, slice] = None
-
-        self.phase_distribution: torch.nn.Parameter = None
-        self._calculated_fitness = 0.
-        self._loss: torch.Tensor = None
-        self.target_torch: torch.Tensor = None
-        self.image_torch: torch.Tensor = None
-
-        self._loss_function:  nn.MSELoss = None
-
-    def value_changed(self, param: Parameter):
-        self.parent_app.algo_settings_changed()
-
-    def do_things_after_init(self):
-        QtWidgets.QApplication.setOverrideCursor(QtCore.Qt.CursorShape.WaitCursor)
-
-        # Initialize phase distribution as trainable parameter
-        self.phase_distribution = torch.nn.Parameter(torch.asarray(self.object_field.phase))
-        self.target_torch = torch.asarray(self._target_field.field)
-        self._loss_function = nn.MSELoss()
-        self.optimizer = optim_ncg.BASIC([self.phase_distribution], method='LS',
-                                         line_search='Armijo', c1=1e-4, c2=0.9, lr=1,
-                                         rho=0.5)  # OK
-        QtWidgets.QApplication.restoreOverrideCursor()
+        self.loss = self.compute_loss_function()
 
     @property
-    def object_torch(self) -> torch.Tensor:
-        return torch.Tensor(torch.asarray(self.object_field.amplitude)) * torch.exp(1j * self.phase_distribution)
+    def phase_distribution(self):
+        return self._phase_distribution.get_value()
 
-    def propagate_field(self):
-        self.image_torch = torch.fft.fft2(self.object_torch)
-        img_array = self.image_torch.numpy(force=True)
-        self._image_field = self.scale_target_with_geometry(
-            Field('image', np.abs(img_array), np.angle(img_array)))
+    @phase_distribution.setter
+    def phase_distribution(self, value: np.ndarray):
+        self._phase_distribution.set_value(value)
 
-
-    def closure(self):
-        self.optimizer.zero_grad()
-
-        if self.apply_mask(apply_to=ApplyMaskTo.TARGET):
-            if self.update_mask:
-
-                self._slices = self.get_mask_slices(ApplyMaskTo.TARGET)
-                self.update_mask = False
-        else:
-            self._slices = (Ellipsis, Ellipsis)
-
-        d=2
-        # max_amplitude = torch.max(torch.abs(self.image_torch[*self._slices])**2 *
-        #                           torch.abs(self.target_torch[*self._slices])**2)
-
-        # loss = 10 ** d * (
-        #         1 - torch.sum(
-        #     torch.sqrt(
-        #         1/ max_amplitude *
-        #         torch.abs(self.image_torch[*self._slices])**2 *
-        #         torch.abs(self.target_torch[*self._slices])**2) *
-        #     torch.cos(torch.angle(self.image_torch[*self._slices]) -
-        #               torch.angle(self.target_torch[*self._slices]))))**2
-
-        loss = self._loss_function(torch.abs(self.image_torch[*self._slices]),
-                                   torch.abs(self.target_torch[*self._slices]))
-        loss.backward(retain_graph=True)
-        self._calculated_fitness = loss.numpy(force=True)
-
-        return loss
-
-    def evolve_field(self):
-
-        self.optimizer.step(self.closure)
-
-        self.set_phase_in_object_plane(self.phase_distribution.data.numpy(force=True))
-
-    def compute_phase(self):
-        self.propagate_field()
-        self.evolve_field()
-
-    @property
-    def fitness(self) -> float:
-        """ Compute fitness with respect to the image_field and target_field """
-        return float(self._calculated_fitness)
+    def compute_loss_function(self) -> type[pt.function]:
+        fft = fft2(self._object_amplitude_array * tensor.cos(self._phase_distribution), norm='ortho')
+        loss = tensor.abs((self.image_field[...,0] - self.target_field.real_field)**2 +
+                          (self.image_field[...,1] - self.target_field.imag_field)**2)
+        return pt.function([], loss)
 
 
 
+if __name__ == '__main__':
+    from pymodaq_gui.utils.dock import DockArea
+
+    from pymodaq_plugins_optical_2D_shaping.field.field_loader_app import FieldLoaderApp
+    from pymodaq_plugins_optical_2D_shaping.field.loaders.save_field_loader import SavedFieldLoader
+    from pymodaq_plugins_optical_2D_shaping.field.loaders.from_functions_loader import GaussianIntensity
+    from qtpy import QtWidgets
+    from pymodaq_data import DataToExport, DataRaw
+
+    app = mkQApp('CG')
+
+    field_loader = FieldLoaderApp(DockArea())
+
+    field_loader.loader = 'GaussianIntensityLoader'
+    field_loader.load_field()
+    object_field = field_loader.field
+    object_field.phase = np.random.rand(*object_field.shape) * 2 * np.pi
+    object_field.amplitude_as_dwa().plot('qt')
+
+    field_loader.loader = 'SavedFieldLoader'
+    field_loader.load_field()
+    ferris_field = field_loader.field
+    ferris_field.amplitude_as_dwa().plot('qt')
+
+    model = Model(object_field, ferris_field)
 
 
+    app.exec()
+
+
+
+# @AlgorithmFactory.register_algorithm()
+# class ConjugateGradient(AlgoBase):
+#     """ Implementation of the ConjugateGradient iterative algorithm to create amplitude and phase modulated
+#     image with phase only spatial light modulators in the Fourier plane of a converging lens
+#
+#     Based on Vol. 25, No. 10 | 15 May 2017 | OPTICS EXPRESS 11695 and implemented here with pytorch
+#
+#     The corresponding experimental setup should define a working light wavelength and a focal length
+#     of the used lens
+#     """
+#
+#     ALGO_NAME = 'ConjugateGradient'
+#     SETUP_TYPE = LensSetup.TwoF
+#     ITERATIVE = True
+#
+#     params = []
+#
+#     def __init__(self, parent: 'AlgoApp' = None):
+#         super().__init__(parent)
+#         self._slices: tuple[slice, slice] = None
+#
+#         self.phase_distribution: torch.nn.Parameter = None
+#         self._calculated_fitness = 0.
+#
+#         self._loss: torch.Tensor = None
+#         self.target_torch: torch.Tensor = None
+#         self.image_torch: torch.Tensor = None
+#
+#
+#     def value_changed(self, param: Parameter):
+#         self.parent_app.algo_settings_changed()
+#
+#     def do_things_after_init(self):
+#         QtWidgets.QApplication.setOverrideCursor(QtCore.Qt.CursorShape.WaitCursor)
+#
+#         # Initialize phase distribution as trainable parameter
+#         self.phase_distribution = torch.nn.Parameter(torch.asarray(self.object_field.phase))
+#         self.target_torch = torch.asarray(self._target_field.field)
+#
+#         QtWidgets.QApplication.restoreOverrideCursor()
+#
+#     @property
+#     def object_torch(self) -> torch.Tensor:
+#         return torch.Tensor(torch.asarray(self.object_field.amplitude)) * torch.exp(1j * self.phase_distribution)
+#
+#     def propagate_field(self):
+#         self.image_torch = torch.fft.fft2(self.object_torch)
+#
+#
+#
+#     def compute_loss(self):
+#
+#         if self.apply_mask(apply_to=ApplyMaskTo.TARGET):
+#             if self.update_mask:
+#
+#                 self._slices = self.get_mask_slices(ApplyMaskTo.TARGET)
+#                 self.update_mask = False
+#         else:
+#             self._slices = (Ellipsis, Ellipsis)
+#
+#         d=2
+#         max_amplitude = torch.max(torch.abs(self.image_torch[*self._slices])**2 *
+#                                   torch.abs(self.target_torch[*self._slices])**2)
+#
+#         loss = 10 ** d * (
+#                 1 - torch.sum(
+#             torch.sqrt(
+#                 1/ max_amplitude *
+#                 torch.abs(self.image_torch[*self._slices])**2 *
+#                 torch.abs(self.target_torch[*self._slices])**2) *
+#             torch.cos(torch.angle(self.image_torch[*self._slices]) -
+#                       torch.angle(self.target_torch[*self._slices]))))**2
+#
+#         loss = (self.image_torch[*self._slices] - self.target_torch[*self._slices]).abs().pow(2).sum()
+#         loss.backward(retain_graph=True)
+#
+#         self._calculated_fitness = loss.item()
+#         return loss
+#
+#     def evolve_field(self):
+#         pass
+#
+#
+#     def compute_phase(self):
+#         self.propagate_field()
+#         self.compute_loss()
+#         self.evolve_field()
+#
+#         self.set_phase_in_object_plane(self.phase_distribution.data.numpy(force=True))
+#         img_array = self.image_torch.numpy(force=True)
+#         self._image_field = self.scale_target_with_geometry(
+#             Field('image', np.abs(img_array), np.angle(img_array)))
+#
+#
+#     @property
+#     def fitness(self) -> float:
+#         """ Compute fitness with respect to the image_field and target_field """
+#         return float(self._calculated_fitness)
+#
+#
+#
+#
+#
---
+++ tests/pytensor_fft.py
@@ -0,0 +1,62 @@
+import numpy as np
+from numpy.fft import fftshift, fft
+from pymodaq_plugins_optical_2D_shaping.field import Field
+from time import perf_counter
+
+
+from pymodaq_gui.utils.utils import mkQApp
+
+app = mkQApp('fft')
+N = 2048
+M = 1024
+box = np.zeros((M, N), dtype='float64')
+box[M//2-10: M//2+10, N//2-50: N//2+50] = 1
+field = Field('box', amplitude=box)
+
+# using numpy
+fft_complex = fftshift(fft(fftshift(field.amplitude, axes=0), axis=0), axes=0)
+fft_2_complex = fftshift(fft(fftshift(fft_complex.T, axes=0), axis=0), axes=0).T
+
+field_fft = Field('fft', amplitude=np.abs(fft_complex), phase=np.angle(fft_complex))
+field_fft2 = Field('fft', amplitude=np.abs(fft_2_complex), phase=np.angle(fft_2_complex))
+
+
+field_fft.intensity_as_dwa().plot('qt')
+field_fft2.intensity_as_dwa().plot('qt')
+
+# using pytensor
+import pytensor
+import pytensor.tensor as pt
+from pytensor.tensor import fft
+
+
+start = perf_counter()
+x = pytensor.shared(box, 'box')
+
+
+rfft = fft.rfft(x, norm='ortho')
+rfft_complex = (rfft[..., 0] + 1j * rfft[..., 1]).transpose()
+rfft2 = fft.rfft(pt.abs(rfft_complex) * pt.cos(pt.angle(rfft_complex)), norm='ortho')
+rfft2_complex = (rfft2[..., 0] + 1j * rfft2[..., 1]).transpose()
+fft2_complex = pt.abs(rfft2_complex) * pt.exp(1j * pt.angle(rfft2_complex))
+
+f_rfft2 = pytensor.function([], fft2_complex)
+
+compiled = perf_counter() - start
+print(f'Compilation time: {compiled}')
+
+calculated = perf_counter()
+for ind in range(100):
+    out = f_rfft2()
+
+    calculated = perf_counter() - calculated
+    print(f'Calculated time: {calculated}')
+
+    print(out.shape)
+
+
+field_fft2_tensor = Field(amplitude=np.abs(out), phase=np.angle(out))
+field_fft2_tensor.intensity_as_dwa('tensor', 'tensor').plot('qt')
+
+
+app.exec()
