b2a6c9b1c4cdfc91c5346792f074e1f856d2588e
Sébastien Weber <seba.weber@gmail.com>
2026-01-28 00:25:07 +0100

    various attemps to make optim works ad in the notebook... to no avail

--- src/pymodaq_plugins_optical_2D_shaping/algorithms/algorithms/optim.py
+++ src/pymodaq_plugins_optical_2D_shaping/algorithms/algorithms/optim.py
@@ -9,7 +9,7 @@ see https://gregorygundersen.com/blog/2022/03/20/conjugate-gradient-descent/
 from typing import Optional, Union
 
 from typing import Union, Tuple, List, TYPE_CHECKING, Any
-
+from qtpy import QtWidgets
 
 import numpy as np
 from torch import nn
@@ -56,7 +56,7 @@ class TorchOptim(Minimize):
     ALGO_NAME = 'TorchOptim'
     SETUP_TYPE = LensSetup.TwoF
     ITERATIVE = True
-    MANUAL_LOOP = True
+    MANUAL_LOOP = False
 
 
     def do_things_after_init(self):
@@ -71,11 +71,13 @@ class TorchOptim(Minimize):
         self.compute_image_field(self._phase_tensor)
         self.update_data(self._phase_tensor.reshape(np.prod(self._phase_tensor.shape)))
 
-        self.optimizer = torch.optim.LBFGS(
-            [self._phase_tensor],
-            lr=0.01, max_iter=self.settings['max_iter'], max_eval=None, tolerance_grad=self.settings['tolerance'],
-            tolerance_change=1e-09,
-            history_size=100, line_search_fn=None)
+        # self.optimizer = torch.optim.LBFGS(
+        #     [self._phase_tensor],
+        #     lr=1.0, max_iter=self.settings['max_iter'],
+        #     max_eval=None,
+        #     tolerance_grad=self.settings['tolerance'],
+        #     tolerance_change=1e-09,
+        #     history_size=50, line_search_fn="strong_wolfe")
         self.iter = 0
 
     def closure(self):
@@ -88,12 +90,52 @@ class TorchOptim(Minimize):
         if ini_phase is not None:
             self.phase_distribution = ini_phase
 
-        self.optimizer.step(self.closure)
+        phase_ini  = torch.rand(self._phase_tensor.shape) * 2 * torch.pi
+        phase = phase_ini.clone().detach().requires_grad_(True)
+        amplitude  =  self._amplitude_tensor.clone().detach()
+        target_tensor = self._target_tensor.clone().detach()
 
-        self.set_phase_in_object_plane(self.phase_distribution)
-        img_array = self.image_tensor.detach().numpy()
-        self._image_field = self.scale_target_with_geometry(
-            Field('image', np.abs(img_array), np.angle(img_array)))
+        if self.apply_mask(apply_to=ApplyMaskTo.TARGET):
+            slices = self.get_mask_slices(ApplyMaskTo.TARGET)
+            mask = torch.tensor(self.get_mask_field(ApplyMaskTo.TARGET).amplitude)
+        else:
+            slices = (Ellipsis, Ellipsis)
+            mask = torch.ones_like(self._amplitude_tensor)
+
+        optimizer = torch.optim.LBFGS(
+            [phase],
+            lr=1.0, max_iter=self.settings['max_iter'],
+            max_eval=None,
+            tolerance_grad=self.settings['tolerance'],
+            tolerance_change=self.settings['tolerance'],
+            history_size=50, line_search_fn="strong_wolfe")
+
+
+        for ind in range(self.settings['max_iter']):
+            def closure():
+                optimizer.zero_grad()
+                field = amplitude * torch.exp(1j * phase)
+                image_tensor = torch.fft.fftshift(
+                    torch.fft.fft2(
+                        field, norm='backward'
+                    )
+                )
+                image_normalized = image_tensor * torch.sum(torch.abs(amplitude)) / torch.sum(torch.abs(image_tensor))
+                diff = image_normalized - target_tensor
+                loss = torch.mean(torch.abs(diff * mask) ** 2)
+                self._calculated_fitness = float(loss)
+                loss.backward()
+                return loss
+
+            optimizer.step(closure)
+
+            print(torch.mean(torch.abs(self._phase_tensor - phase)))
+
+            self.set_phase_in_object_plane(phase.detach().numpy())
+            self.compute_forward_fft(update_plots=True)
+
+            QtWidgets.QApplication.processEvents()
+
 
 
 
