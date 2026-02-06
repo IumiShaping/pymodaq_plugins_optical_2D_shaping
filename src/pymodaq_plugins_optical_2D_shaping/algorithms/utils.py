corrected the quad phase calculation when square fields
using slm size as input of incident light (as it should be anyway)

src/pymodaq_plugins_optical_2D_shaping/algorithms/utils.py
@@ -146,9 +146,11 @@ class AlgoBase(AlgoParameterManager, metaclass=ABCMeta):
     def focal_quad(self) -> np.ndarray:
         """ Compute focal to add in order to have all light on the size of the target """
         focal = Q_(plugin_config('setup', self.SETUP_TYPE.value, 'focals')[0], 'mm')
-        object_size = Q_(np.array([self.object_field.pixels_sizes[ind].magnitude *
-                                   self.object_field.shape[ind] for ind in range(2)]),
-                         self.object_field.pixels_sizes[0].units)
+        object_size = Q_(np.array(sizing.get_effective_slm_size()) * sizing.get_effective_slm_pixel_size(),
+                         'um')
+        # object_size = Q_(np.array([self.object_field.pixels_sizes[ind].magnitude *
+        #                            self.object_field.shape[ind] for ind in range(2)]),
+        #                  self.object_field.pixels_sizes[0].units)
         if self.apply_mask(ApplyMaskTo.TARGET):
             _slices = self.get_mask_slices(ApplyMaskTo.TARGET)
             size = [(_slice.stop - _slice.start)