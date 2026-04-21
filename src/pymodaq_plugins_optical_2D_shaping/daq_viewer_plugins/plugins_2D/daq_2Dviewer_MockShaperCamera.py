7c7d4c414ada98befc0f116aa41871195fea688b
Sébastien Weber <seba.weber@gmail.com>
2026-04-21 17:43:28 +0200

    creating mock environment to test calibration and scripting!

--- src/pymodaq_plugins_optical_2D_shaping/daq_viewer_plugins/plugins_2D/daq_2Dviewer_MockShaperCamera.py
+++ src/pymodaq_plugins_optical_2D_shaping/daq_viewer_plugins/plugins_2D/daq_2Dviewer_MockShaperCamera.py
@@ -1,23 +1,32 @@
-from pathlib import Path
-
 import numpy as np
 
-from pymodaq.utils.daq_utils import ThreadCommand
-from pymodaq.utils.data import DataFromPlugins, Axis, DataToExport
+from pymodaq_utils.utils import ThreadCommand
+from pymodaq_data.data import DataToExport, Axis
+from pymodaq_gui.parameter import Parameter
+
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
-from pymodaq.utils.parameter import Parameter
+from pymodaq.utils.data import DataFromPlugins
 
-from pymodaq_plugins_optical_2D_shaping.algorithms.algorithms.gershberg_saxton import GbSax
+from pymodaq_plugins_optical_2D_shaping.hardware.mock_shaper_camera import ShaperCamera
 
 
-class DAQ_2DViewer_MockHolography(DAQ_Viewer_base):
-    """
+class DAQ_2DViewer_MockShaperCamera(DAQ_Viewer_base):
+    """ Instrument plugin class for a 2D viewer.
+    
+    This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
+    DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
+
+    Attributes:
+    -----------
+    controller: object
+        The particular object that allow the communication with the hardware, in general a python wrapper around the
+         hardware library.
     """
     params = comon_parameters + [
     ]
 
     def ini_attributes(self):
-        self.controller: GbSax = None
+        self.controller: ShaperCamera = None
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -27,8 +36,8 @@ class DAQ_2DViewer_MockHolography(DAQ_Viewer_base):
         param: Parameter
             A given parameter (within detector_settings) whose value has been changed by the user
         """
-        # TODO for your custom plugin
-        pass
+        if param.name() == "a_parameter_you've_added_in_self.params":
+            pass
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -45,13 +54,14 @@ class DAQ_2DViewer_MockHolography(DAQ_Viewer_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        self.ini_detector_init(old_controller=controller,
-                               new_controller=None)
         if self.is_master:
-            self.controller = GbSax()
+            self.controller = ShaperCamera()
+            initialized = True
+        else:
+            self.controller = controller
+            initialized = True
 
-        info = "GBSAX initialized"
-        initialized = True
+        info = ""
         return info, initialized
 
     def close(self):
@@ -69,18 +79,17 @@ class DAQ_2DViewer_MockHolography(DAQ_Viewer_base):
         kwargs: dict
             others optionals arguments
         """
-        self.controller.propagate_field()
-        self.dte_signal.emit(DataToExport(
-            'GBSAX',
-            data=[DataFromPlugins(name='GBSAX Intensity',
-                                  data=[self.controller.intensity_output],
-                                  dim='Data2D', labels=['Field Object intensity']),
-                  ]))
+
+        data_array = self.controller.get_camera()
+        self.dte_signal.emit(DataToExport('FFT data',
+                                          data=[DataFromPlugins(name='Mock1', data=[data_array],
+                                                                dim='Data2D', labels=['FFT data'])]))
+
+
 
     def stop(self):
         """Stop the current grab hardware wise if necessary"""
         pass
-        return ''
 
 
 if __name__ == '__main__':
