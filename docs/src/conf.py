489c593527eed8fc363d6585518dad015cbc8b13
Sébastien Weber <seba.weber@gmail.com>
2026-07-16 18:22:20 +0200

    update logo

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -178,7 +178,7 @@ html_css_files = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_logo = 'splash.png'
+html_logo = 'logo.png'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
