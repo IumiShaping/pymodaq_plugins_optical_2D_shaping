e959e4f585219b40e57d211cf4d3a4519651fbdd
Sébastien Weber <seba.weber@gmail.com>
2026-08-25 16:01:54 +0200

    Update conf.py

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -26,7 +26,6 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
-    "sphinx_qt_documentation",
     "sphinx_design",
     "sphinx_favicon",
     "sphinxext.rediraffe",
